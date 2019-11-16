4de063f073cd9b05ec712e313e8d3d18a0ca6d02
Tomislav <57530939+TV321@users.noreply.github.com>
2019-11-16 18:46:50 +0100

    update

--- script.js
+++ script.js
@@ -4,26 +4,57 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
+ //var rbutton = document.querySelectorAll(".remove");
+var rbutton = document.getElementsByClassName("remove");
+
 function inputLength(){
   return input.value.length;
 }
 
 function createLi(){
+  //createLI
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
+  li.classList.add("li2")
+  //create Button
+  var removeButton = document.createElement("button");
+  removeButton.appendChild(document.createTextNode("Remove button"));
+  removeButton.classList.add("remove2");
+  //createDiv
+  var div = document.createElement("div");
+  div.appendChild(li);
+  div.appendChild(removeButton);
+  ul.appendChild(div);
+  //ul.appendChild(li);
   input.value = "";
 }
 
 function buttonClick(){
   if (inputLength() > 0) {
     createLi();
+
+    var rbutton2 = document.getElementsByClassName("remove2");
+    for (var i = 0; i < rbutton2.length; i++) {
+      rbutton2[i].addEventListener("click", function(){
+        this.parentElement.remove();
+      })
+    }
+
   }
 }
 
 function keyPress(event){
   if (inputLength() > 0 && event.which === 13) {
     createLi();
+
+    var rbutton2 = document.getElementsByClassName("remove2");
+    for (var i = 0; i < rbutton2.length; i++) {
+      rbutton2[i].addEventListener("click", function(){
+        this.parentElement.remove();
+      })
+    }
+
+
   }
 }
 
@@ -33,7 +64,8 @@ input.addEventListener("keypress", keyPress);
 
 /****************************Line through--style ******************************/
 
-var li = document.querySelectorAll("li");
+//var li1 = document.querySelectorAll("li");
+//var li = document.getElementsByTagName("li");
 ////for loop
 /*
 for (var i = 0; i < li.length; i++) {
@@ -44,19 +76,34 @@ for (var i = 0; i < li.length; i++) {
 */
 
 //// forEach method, same stuff
-li.forEach(function(value, index) {
-  li[index].addEventListener("click", function(){
+/*
+  li1.forEach(function(value, index) {
+  li1[index].addEventListener("click", function(){
     this.classList.toggle("done");
   })
-})
+})*/
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle("done");
+  }
+}, false);
+
 
 /**********************Remove on click*****************************************/
 
-var rbutton = document.querySelectorAll(".remove");
-
+/*
 rbutton.forEach(function(value, index) {
   rbutton[index].addEventListener("click", function(){
     this.previousElementSibling.remove();
     this.remove();
   })
 })
+*/
+
+for (var i = 0; i < rbutton.length; i++) {
+  rbutton[i].addEventListener("click", function(){
+    this.previousElementSibling.remove();
+    this.remove();
+  })
+}
